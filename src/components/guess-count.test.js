e98b426b093d21366baf6f12a01adaefaf173749
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-04-19 22:24:54 -0700

    Add test for plural and singular guesses for GuessCount component.

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -7,4 +7,18 @@ describe('<GuessCount />', () => {
     it('Should render without crashing', () => {
         shallow(<GuessCount />);
     });
+
+    it('Should render plural guessNoun', () => {
+        const guessCount = 2;
+        const wrapper = shallow(<GuessCount guessCount={guessCount}/>);
+
+        expect(wrapper.find('h2').text()).toContain('guesses');
+    });
+
+    it('Should render singular guessNoun', () => {
+        const guessCount = 1;
+        const wrapper = shallow(<GuessCount guessCount={guessCount}/>);
+
+        expect(wrapper.find('h2').text()).toEqual("You've made 1 guess!");
+    });
 });
